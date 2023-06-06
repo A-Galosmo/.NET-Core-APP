fcd93dc82b68024d2fde609573c8c7e826bc7c6f
A-Galosmo <112082392+A-Galosmo@users.noreply.github.com>
2023-06-06 11:43:35 +0800

    Second Commit
    Revamped

--- MySQLWebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MySQLWebApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ SQLWebApplication/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.Data.SqlClient;
+
+
+
+static void Main()
+{
+    string connectionString = "Server=testonly.database.windows.net;Database=myTestDB;User Id=akoonly;Password=Number@123!;";
+
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+
+        // Perform a sample query
+        string query = "SELECT * FROM YourTable";
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    // Process the data returned by the query
+                    string columnName = reader.GetString(0);  // Replace 0 with the appropriate column index
+                    Console.WriteLine(columnName);
+                }
+            }
+        }
+    }
+
+    Console.ReadLine();
+}
